404300af8fcad2dfe2e20ca40a3dc8ad2b025899
Dewi Febriani <dewifebriani@tazkia.ac.id>
2026-01-17 11:21:14 +0700

    feat : buat koneksi ke database

---
+++ koneksi.php
@@ -0,0 +1,13 @@
+<?php 
+$host ="localhost";
+$port=3306;
+$user="root";
+$password ="";
+$database="belajarcrud";
+
+$conn = mysqli_connect($host,$user,$password,$database,$port);
+
+if(!$conn){
+    die("koneksi gagal:" .mysqli_connect_error());
+}
+?>
